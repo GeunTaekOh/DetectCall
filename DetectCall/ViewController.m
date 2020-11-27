d142c984e3ca57bc7dad75762950a83e82f0e811
GeunTaekOh <rmsxor94@naver.com>
2020-11-27 09:45:14 +0900

    [버튼 클릭시마다 데이터 동적으로 수정]

--- DetectCall/ViewController.m
+++ DetectCall/ViewController.m
@@ -9,15 +9,17 @@
 
 @interface ViewController ()
 
+
+
 @end
 
 @implementation ViewController
 
+static bool dataChange = YES;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    [self sharingJsonDataToCallKit];
     
 }
 
@@ -36,6 +38,15 @@
     NSMutableArray<NSDictionary*> * dbData = [[NSMutableArray alloc] init];
     
     NSURL * file = [[NSBundle mainBundle] URLForResource:@"DBData" withExtension:@"json"];
+    
+    if(dataChange){
+        file = [[NSBundle mainBundle] URLForResource:@"DBData" withExtension:@"json"];
+        NSLog(@"DETECT // get DBData.json");
+    }else{
+        file = [[NSBundle mainBundle] URLForResource:@"DBData2" withExtension:@"json"];
+        NSLog(@"DETECT // get DBData2.json");
+    }
+    
     NSData * data = [[NSData alloc] initWithContentsOfURL:file];
     NSArray<NSDictionary*> *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
     
@@ -47,6 +58,11 @@
 
 - (IBAction)clickBtn:(id)sender {
     CXCallDirectoryManager * manager = [CXCallDirectoryManager sharedInstance];
+    
+    
+    [self sharingJsonDataToCallKit];
+    
+    dataChange = !dataChange;
     
     [manager reloadExtensionWithIdentifier:@"com.geuntaek.DetectCall.CallDirectoryHandler" completionHandler:^(NSError * _Nullable error) {
         if(error != nil){
